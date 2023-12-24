bf9af620af662485f420b746daa7a2b66e875107
ograsyilmaz <yilmazogras@outlook.com.tr>
2023-12-24 12:04:22 +0300

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,80 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import {
+  ColumnDirective,
+  ColumnsDirective,
+  GridComponent,
+  Inject,
+  Page,
+  Sort,
+  Filter,
+  Group,
+} from "@syncfusion/ej2-react-grids";
+import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
+import { data } from "./datasource";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const pageSettings: object = { pageSize: 6 };
+  const filterSettings: object = { type: "Excel" };
+
+  const [dataList, setDataList] = useState<any[]>([]);
+  const [search, setSearch] = useState<string>("");
+
+  const searchHandler = () => {
+    const filteredSearchData = dataList.filter((item) => {
+      return Object.values(item).some((value) =>
+        String(value).toLowerCase().includes(search.toLowerCase())
+      );
+    });
+    setDataList(filteredSearchData);
+  };
+
+  useEffect(() => {
+    return setDataList(data);
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <>
+      <div style={{ marginTop: 100, width: 300 }}>
+        <input
+          className="e-input"
+          type="text"
+          placeholder="Content to search"
+          value={search}
+          onChange={(e) => {
+            if (e.target.value.trim() === "") {
+              setDataList(data);
+            }
+            setSearch(e.target.value);
+          }}
         />
+        <ButtonComponent onClick={searchHandler}>Search</ButtonComponent>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <h2>Syncfusion React Grid Component</h2>
+      <GridComponent
+        dataSource={dataList}
+        allowGrouping={true}
+        allowSorting={true}
+        allowFiltering={true}
+        allowPaging={true}
+        pageSettings={pageSettings}
+        filterSettings={filterSettings}
+        height={180}
+      >
+        <ColumnsDirective>
+          <ColumnDirective field="OrderID" width="100" textAlign="Right" />
+          <ColumnDirective field="CustomerID" width="100" />
+          <ColumnDirective field="EmployeeID" width="100" textAlign="Right" />
+          <ColumnDirective
+            field="Freight"
+            width="100"
+            format="C2"
+            textAlign="Right"
+          />
+          <ColumnDirective field="ShipCountry" width="100" />
+        </ColumnsDirective>
+        <Inject services={[Page, Sort, Filter, Group]} />
+      </GridComponent>
+    </>
+  );
 }
